21ac74b7b6b343fe677417e9a6820f6a3f7eb1ee
Jack <subbak2@likelion.org>
2022-02-19 00:00:13 +0900

    permit / 220219

--- src/main/java/com/subbak2/www/config/security/WebSecurityConfiguration.java
+++ src/main/java/com/subbak2/www/config/security/WebSecurityConfiguration.java
@@ -2,6 +2,7 @@ package com.subbak2.www.config.security;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -25,5 +26,11 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter{
 		)
 		;
 	}
-	
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+    	http.authorizeRequests()
+        	.antMatchers("/").permitAll()
+            	.anyRequest().authenticated();
+    }
 }
